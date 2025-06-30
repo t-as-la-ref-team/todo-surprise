8d021208d09b31114d5f6c700bd8e5488dfc4bb6
Canolan <nolancano@gmail.com>
2025-06-30 14:05:46 +0200

    test

--- front/cypress/e2e/home.cy.js
+++ front/cypress/e2e/home.cy.js
@@ -1,9 +1,13 @@
 describe('Page /todos - To-Do App', () => {
   it('doit afficher les éléments principaux de la page', () => {
     cy.visit('/todos');
-    cy.contains('My To-Do List'); // corrige la casse
-    cy.contains('Remaining tasks');
-    cy.contains('Completed tasks');
-    cy.contains('No tasks available');
+
+    // attendre que le titre soit visible pour être sûr que la page est bien chargée
+    cy.contains('My To-Do List', { timeout: 10000 }).should('be.visible');
+
+    // Vérifie la présence des textes clés
+    cy.contains('Remaining tasks').should('be.visible');
+    cy.contains('Completed tasks').should('be.visible');
+    cy.contains('No tasks available').should('be.visible');
   });
 });
