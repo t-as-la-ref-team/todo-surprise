0a9fa1af59009b2e71339360d2212d430520d85f
Ambre Rouillon <ambre.rouillon@gmail.com>
2025-07-02 21:07:38 +0200

    renomage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
+  testMatch: ['**/tests/**/*.test.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageThreshold: {
