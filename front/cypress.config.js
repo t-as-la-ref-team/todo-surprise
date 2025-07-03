5703ba2663a1cfd8af5c1b949eea52f8e986b5eb
Canolan <nolancano@gmail.com>
2025-07-03 13:47:39 +0200

    test

--- front/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:4200',
-    supportFile: false,
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
-})
