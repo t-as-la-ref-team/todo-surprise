bf3d5e8b9c965015525858b2a4afa044a024c167
Xeaphones <yohan2003@free.fr>
2025-07-03 13:26:39 +0200

    db fix

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({
-  host: process.env.DB_HOST,
+  host: 'db',
   port: parseInt(process.env.DB_PORT, 10), // convertir en nombre
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASS
 });
 
 db.connect()
